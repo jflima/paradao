231a8ae23d0057b958d69b79a2baf1cd1c8c1115
Angelo Brito <angelo.brito@outlook.com.br>
2016-08-14 00:09:01 -0300

    Updated the url for sensor list to be compliant whit the rest_api. Since it required sensor we had to change the list to sensor_list

--- paradao_server/paradao/paradao/urls.py
+++ paradao_server/paradao/paradao/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include("rest_server.urls")),
+    url(r'^sensor_list', include("sensor.urls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
