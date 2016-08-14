4688cf766105d577d5d09fcd3e23f9ecc7e8f583
Angelo Brito <angelo.brito@outlook.com.br>
2016-08-14 01:36:36 -0300

    Updating sensor

--- paradao_server/paradao/sensor/urls.py
+++ paradao_server/paradao/sensor/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.sensorParser, name='sensorParser'),
+    # /sensorList
+    url(r'^$', views.sensorList, name='sensorList'),
 ]
--- paradao_server/paradao/sensor/views.py
+++ paradao_server/paradao/sensor/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts   import get_object_or_404, render
+from django.http        import HttpResponseRedirect
+from django.views       import generic
+from .models            import Sensor, SensorValue
+from rest_server.models import Parada
 import json, urllib2
 
 # Create your views here.
-def sensorParser(request):
-    jsonStr = urllib2.urlopen('http://hackercidadao.com.br/embarquelab/downloads/EL_sensores_json.php')
-    sensorsDict = json.loads(jsonStr.read())
-    context = {
-        'sensorList' : sensorsDict['Sensor'],
-        'sensorsQtt' : len(sensorsDict['Sensor']),
-    }
-
-    return render(request, 'sensor/sensorList.html', context)
-
+def sensorList(request):
+        jsonStr = urllib2.urlopen('http://hackercidadao.com.br/embarquelab/downloads/EL_sensores_json.php')
+        sensorsDict = json.loads(jsonStr.read())
+        for sensor in sensorsDict['Sensor']:
+            sen = Sensor.objects.filter(codigo=int(sensor['Codigo'])).get()
+            val = SensorValue(sensor=sen, valor=sensor['Valor'])
+            val.save()
+        
+        context = {
+            'sensorList' : sensorsDict['Sensor'],
+            'sensorsQtt' : len(sensorsDict['Sensor']),
+        }
+        return render(request, 'sensor/sensorList.html', context)
 
