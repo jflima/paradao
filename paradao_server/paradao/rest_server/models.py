7bcdcc49023c64f8e189fab0f414c25f75b11124
Jamerson Lima <jamersonfpl@gmail.com>
2016-08-13 22:41:20 -0300

    Modelo e serviço REST de parada.

--- paradao_server/paradao/rest_server/models.py
+++ paradao_server/paradao/rest_server/models.py
@@ -3,7 +3,18 @@ from django.db import models
 
 
 # Create your models here.
+
+class Parada(models.Model):
+    codigo = models.TextField(default='', help_text="Codigo da parada")
+    logradouro = models.TextField(default='', help_text="Logradouro da parada")
+    bairro = models.TextField(default='', help_text="Bairro da parada")
+    cidade = models.TextField(default='', help_text="Cidade da parada")
+    latitude = models.TextField(default='', help_text="Latitude da parada")
+    longitude = models.TextField(default='', help_text="Longitude da parada")
+
+
 class Sensor(models.Model):
+    parada = models.ForeignKey(Parada)
     codigo = models.IntegerField(default=0, help_text="Codigo do sensor")
     porta = models.IntegerField(default=0, help_text="Porta do sensor")
     nome = models.TextField(default='', help_text="Nome do sensor")
--- paradao_server/paradao/rest_server/urls.py
+++ paradao_server/paradao/rest_server/urls.py
@@ -1,24 +1,40 @@
 from django.conf.urls import url, include
+from models import Parada
 from models import Sensor
 from rest_framework import routers, serializers, viewsets
 
 
 # Serializers define the API representation.
+class ParadaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Parada
+        fields = ('codigo', 'logradouro', 'bairro',
+                  'cidade', 'latitude', 'longitude')
+
+
 class SensorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Sensor
         fields = ('codigo', 'porta', 'nome',
                   'descricao', 'localizacao', 'tipo',
-                  'data_sheet', 'minimo', 'maximo', 'valor')
+                  'data_sheet', 'minimo', 'maximo', 'valor',
+                  'parada')
 
 
 # ViewSets define the view behavior.
+class ParadaViewSet(viewsets.ModelViewSet):
+    queryset = Parada.objects.all()
+    serializer_class = ParadaSerializer
+
+
 class SensorViewSet(viewsets.ModelViewSet):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
+router.register(r'parada', ParadaViewSet)
 router.register(r'sensor', SensorViewSet)
 
 
