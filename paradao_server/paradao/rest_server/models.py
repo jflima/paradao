84dec0aa8f9eefd378d9c42adac8a0254e3f72b7
Angelo Brito <angelo.brito@outlook.com.br>
2016-08-14 05:09:16 -0300

    Changed native __str__ function

--- paradao_server/paradao/rest_server/models.py
+++ paradao_server/paradao/rest_server/models.py
@@ -5,9 +5,12 @@ from django.db import models
 # Create your models here.
 
 class Parada(models.Model):
-    codigo = models.TextField(default='', help_text="Codigo da parada")
+    codigo = models.TextField(primary_key=True, default='', help_text="Codigo da parada")
     logradouro = models.TextField(default='', help_text="Logradouro da parada")
     bairro = models.TextField(default='', help_text="Bairro da parada")
     cidade = models.TextField(default='', help_text="Cidade da parada")
     latitude = models.TextField(default='', help_text="Latitude da parada")
     longitude = models.TextField(default='', help_text="Longitude da parada")
+
+    def __str__(self):
+        return "Parada[%s]: latitude=%s longitude=%s"%(self.codigo, self.latitude, self.longitude)
